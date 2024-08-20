6eb8616c7f882c7783344835435f5183deed1ef6
Sebastianmentor <sebastian.ohman@systementor.se>
2024-08-20 11:20:03 +0200

    first commit

---
+++ car.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self, regnr:str, brand:str, fuel:str) -> None:
+        self.regnr = regnr
+        self.brand = brand
+        self.fuel = fuel
---
+++ main.py
@@ -0,0 +1,93 @@
+from car import Car
+from timeit import default_timer
+import random
+import string
+import sys
+
+
+ALPHABET = list(string.ascii_uppercase)
+DIGITS = list(string.digits)
+
+
+
+def create_all_car_with_all_license_plates() -> list[Car]:
+    l = []
+
+    for first_pos in ALPHABET:
+        for second_pos in ALPHABET:
+            for third_pos in ALPHABET:
+                for i in DIGITS:
+                    for j in DIGITS:
+                        for k in DIGITS:
+                            regnr = first_pos + second_pos + third_pos + i + j + k
+                            l.append(Car(regnr, '', ''))
+
+    return l
+
+def create_all_car_with_all_license_plates_dict() -> list[Car]:
+    d = {}
+
+    for first_pos in ALPHABET:
+        for second_pos in ALPHABET:
+            for third_pos in ALPHABET:
+                for i in DIGITS:
+                    for j in DIGITS:
+                        for k in DIGITS:
+                            regnr = first_pos + second_pos + third_pos + i + j + k
+                            d[regnr] = Car(regnr, '', '')
+
+    return d
+
+def get_car_by_regno(regnr:str, list_of_cars:list[Car]) -> Car|None:
+
+    for car in list_of_cars:
+        if car.regnr == regnr:
+            return car
+
+    return None    
+
+def get_car_by_regno_dict(regnr:str, dict_of_cars:dict[str,Car]) -> Car|None:
+
+    if regnr in dict_of_cars: 
+        return dict_of_cars[regnr]
+    
+    return None    
+
+def main():
+    t_start = default_timer()
+    cars_list = create_all_car_with_all_license_plates()
+    t_end = default_timer()
+    print(f'Det tog {t_end - t_start} att skapa listan och tar upp {sys.getsizeof(cars_list)//10**6}!')
+    random.shuffle(cars_list)
+    print(cars_list[0].regnr, cars_list[1000].regnr, cars_list[7000000].regnr, cars_list[-1].regnr)
+
+    t_start = default_timer()
+    cars_dict = create_all_car_with_all_license_plates_dict()
+    t_end = default_timer()
+    print(f'Det tog {t_end - t_start} att skapa listan och tar upp {sys.getsizeof(cars_dict)//10**6}!')
+
+
+    while True:
+        regnr = input('Ange regnummer:')
+        if regnr == 'q': break
+        
+        t_start = default_timer()
+        car = get_car_by_regno_dict(regnr, cars_dict)
+        t_end = default_timer()
+        print(f'Sökningen tog {t_end-t_start}')
+
+
+
+        if car:
+            print('Hitta bil!')
+        else:
+            print('Bil finns inte :( ')
+    
+        
+
+
+
+
+
+if __name__ == '__main__':
+    main()
